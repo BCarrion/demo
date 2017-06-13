a9cf84162675602fc85671cd700cf6f297247832
BCarrion <carrion.bernardo@gmail.com>
2017-06-12 19:09:22 -0500

    Parcial 31

---
+++ protected/models/RoleForm.php
@@ -0,0 +1,22 @@
+<?
+/**
+ *
+ */
+class RoleForm extends CFormModel
+{
+
+  #Creamos las propiedades que va a tener el formulario
+  public static $types = array('Operation', 'Task', 'Role');
+  public $name;
+  public $description;
+  public $type = 2;
+
+  public function rules()
+  {
+    return array(
+      array('name, type', 'required'),
+      array('description', 'safe'),
+    );
+  }
+
+}
